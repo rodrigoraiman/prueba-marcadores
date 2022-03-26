13eee2fd0ba0394a6b497867349dd8d2edc96c60
rodrigoraiman <rodrigoraiman@gmail.com>
2022-03-26 03:47:57 -0300

    se agrego url

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
 gem 'bootstrap'
 gem 'jquery-rails'
+gem 'jquery-turbolinks'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_044636) do
+ActiveRecord::Schema.define(version: 2022_03_26_064405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_03_26_044636) do
     t.bigint "type_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
     t.index ["category_id"], name: "index_bookmarks_on_category_id"
     t.index ["type_id"], name: "index_bookmarks_on_type_id"
   end
